424ea93c18a3fcc0a2716478ccf6f83f7589c97d
nickvirtue <nick@mahuika.co.nz>
2017-07-13 00:29:17 +1200

    Import draft editor from @components

--- packages/richtext/src/index.js
+++ packages/richtext/src/index.js
@@ -1,5 +1,5 @@
 import { version as VERSION } from '../package.json' // eslint-disable-line import/no-unresolved
-import TextEditor from './TextEditor'
+import TextEditor from './reduxForm/TextEditor'
 import TextView from './TextView'
 
 module.exports = {
--- packages/richtext/src/reduxForm/mapError.js
+++ packages/richtext/src/reduxForm/mapError.js
@@ -1,8 +1,9 @@
 const mapError = ({ meta: { touched, error, warning } = {}, input: { ...inputProps }, ...props }, errorProp = 'errorText') =>
   (touched && (error || warning)
       ? {
-        ...props, ...inputProps,
-        [errorProp]: error || warning
+        ...props,
+        ...inputProps,
+        [errorProp]: error || warning,
       }
       : { ...inputProps, ...props }
   )
