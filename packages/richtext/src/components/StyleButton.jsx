d0379710e24a58a52567783c1b26dc6498ad3428
nickvirtue <nick@mahuika.co.nz>
2017-07-13 17:28:39 +1200

    Refactor DraftEditor to use JSS

--- packages/richtext/src/components/StyleButton.jsx
+++ packages/richtext/src/components/StyleButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import IconButton from 'material-ui/IconButton'
 import compose from 'recompose/compose'
+import mapProps from 'recompose/mapProps'
 import { injectButtonStyles } from './EditorStyles'
 
 const preventDefault = event => event.preventDefault()
@@ -13,23 +14,15 @@ const wrapPrevent = callback =>
 
 
 const StyleButton = ({
-  toggleStyle,
-  inlineStyle,
-  children,
-
   styles,
-  theme,
-  prepareStyles,
-
-  editorState: omitEditorState,
-  focused: omitFocused,
-
-  ...other,
+  onTouchTap,
+  children,
+  ...other
 }) =>
   <IconButton
     iconStyle={styles.icon}
     style={styles.button}
-    onTouchTap={wrapPrevent(() => toggleStyle(inlineStyle))}
+    onTouchTap={onTouchTap}
     onMouseDown={preventDefault}
     {...other}
   >
@@ -41,4 +34,16 @@ const StyleButton = ({
 
 export default compose(
   injectButtonStyles,
+  mapProps(({
+    toggleStyle,
+    inlineStyle,
+    children,
+    styles,
+    ...other
+  }) => ({
+    styles,
+    onTouchTap: wrapPrevent(() => toggleStyle(inlineStyle)),
+    children,
+    ...other,
+  }))
 )(StyleButton)
