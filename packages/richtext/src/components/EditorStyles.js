e1a388b2fc56a042de262abf2f44f86e7dc82961
nickvirtue <nick@mahuika.co.nz>
2017-07-13 12:51:28 +1200

    Refactor DraftEditor

--- packages/richtext/src/components/EditorStyles.js
+++ packages/richtext/src/components/EditorStyles.js
@@ -61,3 +61,64 @@ export const injectLabelStyles = compose(withStyles(({
     ...shrinkStyle,
   } : {}),
 })))
+
+export const injectUnderlineStyles = compose(withStyles(({
+  textField: {
+    borderColor,
+    disabledTextColor,
+    errorColor,
+    focusColor,
+  },
+}, {
+  disabled,
+  disabledStyle = {},
+  errorText,
+  errorStyle = {},
+  focus,
+  focusStyle = {},
+  style,
+}) => {
+  const { color: errorStyleColor } = errorStyle
+
+  const styles = {
+    root: {
+      border: 'none',
+      borderBottom: 'solid 1px',
+      borderColor,
+      bottom: 12,
+      boxSizing: 'content-box',
+      margin: 0,
+      position: 'absolute',
+      width: '100%',
+      transition: 'all 450ms ease-out',
+    },
+    disabled: {
+      borderBottom: 'dotted 2px',
+      borderColor: disabledTextColor,
+      cursor: 'not-allowed',
+    },
+    focus: {
+      borderBottom: 'solid 2px',
+      borderColor: focusColor,
+      transform: 'scaleX(0)',
+
+    },
+    error: {
+      borderColor: errorStyleColor || errorColor,
+      transform: 'scaleX(1)',
+    },
+  }
+
+  let underline = Object.assign({}, styles.root, style)
+  let focusedUnderline = Object.assign({}, underline, styles.focus, focusStyle)
+
+  if (disabled) underline = Object.assign({}, underline, styles.disabled, disabledStyle)
+  if (focus) focusedUnderline = Object.assign({}, focusedUnderline, {transform: 'scaleX(1)' })
+  if (errorText) focusedUnderline = Object.assign({}, focusedUnderline, styles.error)
+
+  return ({
+    underline,
+    focusedUnderline,
+  })
+}))
+
