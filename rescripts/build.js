5ee1c3becc7e4da92aaded24bc311cff1bc84037
nickvirtue <nick@mahuika.co.nz>
2017-07-15 09:40:25 +1200

    Change build scripts to match recompose style

--- rescripts/build.js
+++ rescripts/build.js
@@ -6,113 +6,42 @@ const { exit, rm, cp, test } = require('shelljs')
 const readline = require('readline-sync')
 const semver = require('semver')
 const glob = require('glob')
-
-const BIN = './node_modules/.bin'
+const cleanDestination = require('./build/cleanDestination').default
+const compileSources = require('./build/compileSources').default
+const copyAdditionalFiles = require('./build/copyAdditionalFiles').default
 
 const {
-  getPackageNames,
-  getSourceDir,
-  getOutDir,
   getSrcPackages,
 } = require('./packageUtils')
 
 const {
   log,
+  logBr,
   logSuccess,
   logError,
   logTitle,
-
-  exec,
-  execLoud,
-
-  writeFile,
+  logPackage,
 } = require('./utils')
 
-// const BASE_PACKAGE_LOC = '../src/basePackage.json'
-
-const cleanDestination = (mhPackage) => {
-  if (mhPackage.out.exists) {
-    log('Deleting compiled package...')
-    rm('-rf', mhPackage.out.dir)
-  }
-}
-
-const compileSources = (packageData) => {
-  const sourceDir = packageData.src.dir
-  const outDir = packageData.out.dir
-
-  log('Compiling package...')
-
-  const sourceFiles = glob
-    .sync(`${sourceDir}/**/*+(js|jsx)`, {
-      ignore: `${sourceDir}/node_modules/**/*.js`,
-    })
-    .map(to => path.relative(sourceDir, to))
-
-  const bCommand = `cd ${sourceDir} && ` +
-    'cross-env BABEL_ENV=cjs ' +
-    `${path.resolve(BIN)}/babel ${sourceFiles.join(' ')} ` +
-    `--out-dir ${path.resolve(outDir)}`
-  const bCode = exec(bCommand)
-  if (bCode !== 0) {
-    logError('...failed')
-    exit(execLoud(bCommand))
-  }
-}
-
-const copyAdditionalFiles = (packageData) => {
-  const sourceDir = packageData.src.dir
-  const outDir = packageData.out.dir
-
-  log('Copying additional package files...')
-  const additionalProjectFiles = ['README.md', '.npmignore', 'package.json']
-  additionalProjectFiles.forEach((filename) => {
-    const src = path.resolve(sourceDir, filename)
-
-    if (! test('-e', src)) return
-
-    cp('-Rf', src, outDir)
-  })
-}
-
 const buildPackage = (mhPackage) => {
-  logTitle(`  Building ${mhPackage.npmName}...  `)
-
-  // const srcPackageJson = path.resolve(sourceDir, 'package.json')
-  // const version = JSON.parse(fs.readFileSync(srcPackageJson, 'utf8').trim()).version
+  logBr()
+  logPackage(`        ${mhPackage.npmName}        `)
 
   cleanDestination(mhPackage)
-
   compileSources(mhPackage)
-
   copyAdditionalFiles(mhPackage)
 
-
-//   log('Generating package.json...')
-//   const packageConfig = Object.assign(
-//     require(BASE_PACKAGE_LOC),
-//     require(path.resolve(sourceDir, 'package.json'))
-//   )
-//
-//   writeFile(
-//     path.resolve(outDir, 'package.json'),
-//     JSON.stringify(packageConfig, null, 2)
-//   )
-//
-//   logSuccess(`  ${packageName} built successfully  `)
+  logSuccess('Done!')
+  logBr()
 }
 
 try {
-  // const packageNames = getPackageNames()
-  // packageNames.forEach(buildPackage)
+  logBr()
+  logTitle('                                            ')
+  logTitle('        Building @mindhive/packages.        ')
+  logTitle('                                            ')
+  logBr()
   getSrcPackages().forEach(buildPackage)
-  // const packageName = `${process.argv[2]}`
-  // if (packageName) {
-  //   buildPackage(packageName)
-  // } else {
-  //   const packageNames = getPackageNames()
-  //   packageNames.forEach(buildPackage)
-  // }
 } catch (error) {
   logError('Release failed due to an error', error)
 }
--- rescripts/utils.js
+++ rescripts/utils.js
@@ -7,10 +7,14 @@ const { flowRight: compose } = require('lodash')
 
 const consoleLog = console.log.bind(console)
 
+exports.NODE_BIN = './node_modules/.bin'
+
 exports.log = compose(consoleLog, chalk.white)
-exports.logSuccess = compose(consoleLog, chalk.reset.inverse.bold.white)
+exports.logBr = () => exports.log(' ')
+exports.logSuccess = compose(consoleLog, chalk.green.bold)
 exports.logError = compose(consoleLog, chalk.red.bold)
-exports.logTitle = compose(consoleLog, chalk.reset.bold.white.bgMagenta)
+exports.logPackage = compose(consoleLog, chalk.reset.bold.white.bgMagenta)
+exports.logTitle = compose(consoleLog, chalk.reset.bold.magenta.bgWhite)
 
 exports.exec = command => execJs(command, { silent: true }).code
 exports.execLoud = command => execJs(command).code
@@ -19,3 +23,14 @@ exports.execLoud = command => execJs(command).code
 exports.writeFile = (filepath, string) =>
   fs.writeFileSync(filepath, string, 'utf8')
 
+//   log('Generating package.json...')
+//   const packageConfig = Object.assign(
+//     require(BASE_PACKAGE_LOC),
+//     require(path.resolve(sourceDir, 'package.json'))
+//   )
+//
+//   writeFile(
+//     path.resolve(outDir, 'package.json'),
+//     JSON.stringify(packageConfig, null, 2)
+//   )
+//
