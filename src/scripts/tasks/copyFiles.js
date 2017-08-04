e5d4d56ed10b076df44c23d87f94915e2a21a2da
nickvirtue <nick@mahuika.co.nz>
2017-08-04 15:25:13 +1200

    Fix copyFiles

--- src/scripts/tasks/copyFiles.js
+++ src/scripts/tasks/copyFiles.js
@@ -7,11 +7,30 @@ export default async ({ buildLocation, sourceLocation, name }, files, logger) =>
   logger.silly('copyFiles', files)
   logger.verbose(name, 'Copying additional package files...')
 
+  logger.addWork(files.length)
+
   files.forEach(async file => {
     const sourceFile = path.resolve(sourceLocation, file)
     if (await fs.pathExists(sourceFile)) {
+      logger.verbose(name, `Copying ${sourceFile}`)
       const destFile = path.resolve(buildLocation, file)
-      fs.copy(sourceFile, destFile)
+      await fs.copy(sourceFile, destFile)
+      logger.verbose(name, 'Copied!')
+      logger.completeWork(1)
     }
   })
+
 }
+
+// export default async ({ buildLocation, sourceLocation, name }, files, logger) => {
+//   logger.silly('copyFiles', files)
+//   logger.verbose(name, 'Copying additional package files...')
+//
+//   files.forEach(async file => {
+//     const sourceFile = path.resolve(sourceLocation, file)
+//     if (await fs.pathExists(sourceFile)) {
+//       const destFile = path.resolve(buildLocation, file)
+//       fs.copy(sourceFile, destFile)
+//     }
+//   })
+// }
